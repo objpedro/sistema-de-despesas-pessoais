08eb0c79b9dd0776ab8454ffb9a0d6d88bfc649d
Pedro Asevedo <pedrovictortec.ti@gmail.com>
2023-11-02 18:35:33 -0300

    graficos e progressbar

--- despesas/main.py
+++ despesas/main.py
@@ -2,6 +2,10 @@ from tkinter import*
 from tkinter import Tk, StringVar, ttk
 from tkinter import messagebox
 from PIL import Image, ImageTk
+from tkinter.ttk import Progressbar
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.pyplot as plt
+from matplotlib.figure import Figure
 
 ################# cores ###############
 co0 = "#2e2d2b"  # Preta
@@ -48,5 +52,129 @@ app_img = ImageTk.PhotoImage(app_img)
 app_logo = Label(frameCima, image=app_img, text=" Orçamento pessoal", width=900, compound=LEFT, padx=5, relief=RAISED, anchor=NW, font=('Verdana 20 bold'),bg=co1, fg=co4 )
 
 app_logo.place(x=0, y=0)
+
+# percentagem ------------------------------------
+
+def percentagem():
+    l_nome = Label(frameMeio, text="Porcentagem da receita gasta", height=1,anchor=NW, font=('Verdana 12 '), bg=co1, fg=co4)
+    l_nome.place(x=7, y=5)
+
+
+    style = ttk.Style()
+    style.theme_use('default')
+    style.configure("black.Horizontal.TProgressbar", background='#daed6b')
+    style.configure("TProgressbar", thickness=25)
+
+    bar = Progressbar(frameMeio, length=180,style='black.Horizontal.TProgressbar')
+    bar.place(x=10, y=35)
+    bar['value'] = 50
+
+    valor = 50
+    print(valor)
+    l_percentagem = Label(frameMeio, text='{:,.2f} %'.format(valor), height=1,anchor=NW, font=('Verdana 12 '), bg=co1, fg=co4)
+    l_percentagem.place(x=200, y=35)
+
+percentagem()
+
+
+# funcao para grafico bar ------------------------
+
+def grafico_bar():
+    # obtendo valores de meses
+    lista_meses = ['Renda', 'Despesa', 'Saldo']
+    lista_valores = [345,225,534]
+
+    # faça figura e atribua objetos de eixo
+    figura = plt.Figure(figsize=(4, 3.45), dpi=60)
+    ax = figura.add_subplot(111)
+    # ax.autoscale(enable=True, axis='both', tight=None)
+    ax.bar(lista_meses, lista_valores,  color=colors, width=0.9)
+
+    # create a list to collect the plt.patches data
+    c = 0
+
+    # set individual bar lables using above list
+    for i in ax.patches:
+        # get_x pulls left or right; get_height pushes up or down
+        ax.text(i.get_x()-.001, i.get_height()+.5,
+                str("{:,.0f}".format(lista_valores[c])), fontsize=17, fontstyle='italic',  verticalalignment='bottom',color='dimgrey')
+
+        c += 1
+
+
+    ax.set_xticklabels(lista_meses,fontsize=16)
+    ax.patch.set_facecolor('#ffffff')
+    ax.spines['bottom'].set_color('#CCCCCC')
+    ax.spines['bottom'].set_linewidth(1)
+    ax.spines['right'].set_linewidth(0)
+    ax.spines['top'].set_linewidth(0)
+    ax.spines['left'].set_color('#CCCCCC')
+    ax.spines['left'].set_linewidth(1)
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    ax.tick_params(bottom=False, left=False)
+    ax.set_axisbelow(True)
+    ax.yaxis.grid(False, color='#EEEEEE')
+    ax.xaxis.grid(False)
+
+    canva = FigureCanvasTkAgg(figura, frameMeio)
+    canva.get_tk_widget().place(x=10, y=70)
+
+grafico_bar()
+
+
+# -----------------------------------------------------------------------------------------
+# funcao de resumo total
+def resumo():
+
+    valor = [345,225,534]
+
+    l_linha = Label(frameMeio, text="", width=215, height=1,anchor=NW, font=('arial 1 '), bg='#545454',)
+    l_linha.place(x=309, y=52)
+    l_sumario = Label(frameMeio, text="Total Renda Mensal      ".upper(), height=1,anchor=NW, font=('Verdana 12'), bg=co1, fg='#83a9e6')
+    l_sumario.place(x=306, y=35)
+    l_sumario = Label(frameMeio, text='R$ {:,.2f}'.format(valor[0]), height=1,anchor=NW, font=('arial 17 '), bg=co1, fg='#545454')
+    l_sumario.place(x=306, y=70)
+
+    l_linha = Label(frameMeio, text="", width=215, height=1,anchor=NW, font=('arial 1 '), bg='#545454',)
+    l_linha.place(x=309, y=132)
+    l_sumario = Label(frameMeio, text="Total Despesas Mensais".upper(), height=1,anchor=NW, font=('Verdana 12'), bg=co1, fg='#83a9e6')
+    l_sumario.place(x=306, y=115)
+    l_sumario = Label(frameMeio, text='R$ {:,.2f}'.format(valor[1]), height=1,anchor=NW, font=('arial 17 '), bg=co1, fg='#545454')
+    l_sumario.place(x=306, y=150)
+
+    l_linha = Label(frameMeio, text="", width=215, height=1,anchor=NW, font=('arial 1 '), bg='#545454',)
+    l_linha.place(x=309, y=207)
+    l_sumario = Label(frameMeio, text="Total Saldo da Caixa    ".upper(), height=1,anchor=NW, font=('Verdana 12'), bg=co1, fg='#83a9e6')
+    l_sumario.place(x=306, y=190)
+    l_sumario = Label(frameMeio, text='R$ {:,.2f}'.format(valor[2]), height=1,anchor=NW, font=('arial 17 '), bg=co1, fg='#545454')
+    l_sumario.place(x=306, y=220)
+
+resumo()
+
+
+# ----------------------------------------------------------------------------------------
+
+# funcao grafico pie
+def grafico_pie():
+    # faça figura e atribua objetos de eixo
+    figura = plt.Figure(figsize=(5, 3), dpi=90)
+    ax = figura.add_subplot(111)
+    lista_valores = [345,225,534]
+    lista_categorias = ['Renda', 'Despesa', 'Saldo']
+
+    # only "explode" the 2nd slice (i.e. 'Hogs')
+    explode = []
+    for i in lista_categorias:
+        explode.append(0.05)
+    ax.pie(lista_valores, explode=explode, wedgeprops=dict(width=0.2), autopct='%1.1f%%', colors=colors,shadow=True, startangle=90)
+    ax.legend(lista_categorias, loc="center right", bbox_to_anchor=(1.55, 0.50))
+
+    canva_categoria = FigureCanvasTkAgg(figura, frame_gra_2)
+    canva_categoria.get_tk_widget().grid(row=0,column=0)
+
+grafico_pie()
+
 
 janela.mainloop ()
